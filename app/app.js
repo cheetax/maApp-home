f4122d2850ab03478a3bbb5e860c3c336f504b51
cheetax <dmitriy.grebenev@gmail.com>
2017-11-23 16:19:26 +0300

    new

---
+++ app/app.js
@@ -0,0 +1,19 @@
+import { AppRegistry } from 'react-native';
+import ShellView from "./views/ShellView"
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../reducers/index";
+import React, { Component } from 'react';
+
+const store = createStore(rootReducer);
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <Provider store={store}>
+        <ShellView />
+      </Provider>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 import { AppRegistry } from 'react-native';
-import ShellView from './Views/ShellView';
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers/index";
-import React, { Component } from 'react';
-
-const store = createStore(rootReducer);
-
-class App extends Component {
-
-  render() {
-    return (
-      <Provider store={store}>
-        <ShellView />
-      </Provider>
-    );
-  }
-}
+import App from "./app/app"
 
 AppRegistry.registerComponent('myApp', () => App);
