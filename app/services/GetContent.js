9587be00750f14c09b88d1e09d5215365c9653b9
Дмитрий Гребенев <dmitriy.grebenev@gmail.com>
2017-12-02 02:19:42 +0300

    new

--- app/services/GetContent.js
+++ app/services/GetContent.js
@@ -12,17 +12,28 @@ var body = "UserName=cheetax&Password=Kalevala";
 var url = "https://magi.mobi";
 var urlLogin = "/auth/credentials";
 var urlExperience = '/clan/treasures/history/tmp_experience/';
+var urlClan = '/clan/235/'
 
 var DomParser = require('react-native-html-parser').DOMParser
 var users = [];
 
 export default async function GetContent() {
-    
-    let getUsers = async (wizards) => {
+
+    let getUsersExp = async (wizards) => {
 
         for (var i = 0; i < wizards.length; i++) {
 
             let href = wizards[i].childNodes[1].getElementsBySelector('a.tdn.c_user')[0].getAttribute('href'); //Id
+            
+        }
+        return users;
+    }
+
+    let getUsers = async (wizards) => {
+
+        for (var i = 0; i < wizards.length; i++) {
+
+            let href = wizards[i].getAttribute('href'); //Id
             await getUser(href).then((user) => {
                 users.push(user);
             }, (error) => {
@@ -38,11 +49,25 @@ export default async function GetContent() {
         console.log(error);
     });
 
-    let get = (i) => new Promise(function (succeed, fail) {
+    let getExperience = (i) => new Promise(function (succeed, fail) {
         httpClient.get(url + urlExperience + i).then((_html) => {
             let doc = new DomParser().parseFromString(_html, 'text/html');
             //console.log(doc);
-            succeed(doc.documentElement.getElementsBySelector('tr'));
+            let elem = doc.documentElement.getElementsBySelector('tr');
+            succeed(elem);
+        }, (error) => {
+            fail(console.log(error));
+        });
+    }, (error) => {
+        console.log(error);
+    });
+
+    let getClans = (i) => new Promise(function (succeed, fail) {
+        httpClient.get(url + urlClan + i).then((_html) => {
+            let doc = new DomParser().parseFromString(_html, 'text/html');
+            //console.log(doc);
+            let elem = doc.documentElement.getElementsBySelector('a.tdn.bl.ptb2.c_89');
+            succeed(elem);
         }, (error) => {
             fail(console.log(error));
         });
@@ -53,18 +78,33 @@ export default async function GetContent() {
     let getUser = async (href) => new Promise(function (succeed, fail) {
         httpClient.get(url + href).then((html) => {
             // console.log(_html)
-            var userDoc = parser.parseFromString(html, 'text/html')
+            var userDoc = parser.parseFromString(html, 'application/xhtml+xml');
+            //var userDoc = parser.parseHtmlSpecialContent(html);
             var name = userDoc.documentElement.getElementsBySelector('div.p_end')[0].textContent;
-            //name = name.remove(name.indexOf('(')-1);
+            name = name.substring(0, name.indexOf('(') - 1);
             var rank = userDoc.documentElement.getElementsBySelector('div.c_orange.normal')[0].textContent;
             rankArray = rank.split('\n');
             rank = rankArray.length === 4 ? rankArray[2].trim() : rankArray[3].trim();
             var dayOfClan = userDoc.documentElement.getElementsBySelector('div.ptb9')[1].textContent;
             dayOfClan = dayOfClan.substring(dayOfClan.indexOf(":") + 1).trim()
+            var param = userDoc.documentElement.getElementsBySelector('div.mt10.mb10.pr10');
+            if (param.length === 0) {
+                param = userDoc.documentElement.getElementsBySelector('div.column1');
+            }
+            var paramText = param[0].textContent;
+            var paramArray = paramText.split('\n');
+            var force = parseInt(paramArray[1].replace('Сила: ', ''));
+            var health = parseInt(paramArray[3].replace('Здоровье: ', ''));
+            var armor = parseInt(paramArray[5].replace('Броня: ', ''));
+            var option = parseInt(((force + health + armor) / 3).toFixed(0));
             var user = {
                 id: href,
                 name: name,
                 rank: rank,
+                force: force,
+                health: health,
+                armor: armor,
+                option: option,
                 dayOfClan: dayOfClan,
             }
             succeed(user);
@@ -81,7 +121,7 @@ export default async function GetContent() {
 
     for (let i = 1; i <= 6; i++) {
         console.log(i);
-        await get(i).then(async (wizards) => {
+        await getClans(i).then(async (wizards) => {
             //console.log(wizards);
             let users = await getUsers(wizards);
         }, (error) => {
