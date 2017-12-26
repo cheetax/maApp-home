495904fe59aac768c09294bc856bb6b760d4dac3
cheetax <dmitriy.grebenev@gmail.com>
2017-12-26 17:18:30 +0300

    new

--- app/views/RulesPage.js
+++ app/views/RulesPage.js
@@ -95,7 +95,7 @@ class RulesPage extends Component {
           name='md-add'
           type='ionicon'
           color='#f50'
-          onPress={() => dispatch({
+          onPress={() => this.props.dispatch({
             type: 'ADD_RULES',
             payload: this.state
           })}
