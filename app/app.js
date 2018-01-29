e8e7bebe3867a1ea9f3b915912056af27432bbac
Дмитрий Гребенев <dmitriy.grebenev@gmail.com>
2018-01-29 23:03:04 +0300

    new

--- app/app.js
+++ app/app.js
@@ -8,42 +8,17 @@ import ShellPage from './views/ShellPage';
 import { getContent, saveRules } from './actions/actionUsers';
 import React, { Component } from 'react';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-// let init = () => new Promise((succes, fail) => {
-//   store.dispatch(getContent());
-//   succes();
-// }).then(() => {
-//   store.subscribe(() => {
-//     saveRules(store.getState().rules)
-//   })
-// });
+const customMiddleware = store => next => action => {
+   console.log(action)
+   return next(action)
+}
 
-//init();
+const store = createStore(rootReducer, applyMiddleware(thunk, customMiddleware));
+
+
 
 export default class App extends Component {
   // constructor(props){
-  //   super(props)
-  //   //init();
-  // }
-  // static navigationOptions = {
-  //   title: 'Battle of Wizards Assistans',
-  //   headerStyle: {
-  //     backgroundColor: '#03A9F4',
-  //     height: 30,
-  //     justifyContent: 'center',
-  //     //alignItems: 'center'
-  //   },
-  //   headerTitleStyle: {
-  //     fontSize: 18,
-  //     color: '#fff',
-  //     textAlign: 'center',
-  //     justifyContent: 'center',
-  //     alignItems: 'center',
-  //     //   margin: 5,
-
-  //   },
-
-  // }
 
   componentWillMount(){
     store.dispatch(getContent())
