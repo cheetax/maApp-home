5226bf49e5f51fbdfc29b2ad39749e0374644847
cheetax <dmitriy.grebenev@gmail.com>
2018-02-13 14:05:03 +0300

    new

--- index.android.js
+++ index.android.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from "./app/app"
 //import { StackNavigator } from 'react-navigation';
 
-AppRegistry.registerComponent('myApp', () => App);
+AppRegistry.registerComponent('BoW Assist', () => App);
